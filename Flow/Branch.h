fd732e0f71f18173367399dd6bb83850de6adaf1
CLiF <clif1593@gmail.com>
2023-04-26 18:41:45 +0900

    flow and branch set

--- Flow/Branch.h
+++ Flow/Branch.h
@@ -2,23 +2,25 @@
 #include <vector>
 #include <string>
 #include "History.h"
-using BranchID = int;
+using BranchID = unsigned int;
 
 // Branch Management Class
 class Branch {
 private:
-	BranchID id;
+	BranchID id; // Unique id of branch (Main Branch : Always 0)
 
 	std::vector<BranchID> parent_brach_id;
 	std::vector<BranchID> child_branch_id;
 	History history;
+
+	std::string file_path; // Absolute file path of the branch file (path//[NAME].flow)
 
 public:
 	Branch();
 	~Branch();
 
 	int LoadBranch(std::string path); // Open Branch File
-	int SaveBranch(std::string path); // Save Branch File
+	int SaveBranch(); // Save Branch File
 
 	const int GetBranchID() const;
 };
--- Flow/Flow.h
+++ Flow/Flow.h
@@ -1,15 +1,24 @@
 #pragma once
 #include <map>
+#include <string>
 #include "Branch.h"
+
+using FlowID = unsigned long long int;
 
 // Flow Main Class
 class Flow {
 private:
 	std::map<BranchID, Branch> branch_table; // Branch table with key (branch_table[key] = Branch)
+	std::string file_path; // Absolute file path of the flow file (path//[NAME].flow)
+	
+	FlowID id; // Unique id of Flow project
 
 public:
 	Flow();
 	~Flow();
+
+	int LoadFlow(std::string path); //Open Flow File
+	int SaveFlow(); //Save Flow File
 
 	Branch* operator[](BranchID id); // Get Branch with id
 
